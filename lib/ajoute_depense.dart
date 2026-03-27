36efa3d2a8342a40d0aa259d74783b2cdf89bc2f
lebeaugs <ianhafiz9999@gmail.com>
2026-03-27 09:48:18 +0100

    feat:navigation vers les nouveaux écrans

---
+++ lib/ajoute_depense.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AjouterDepense extends StatelessWidget {
+  const AjouterDepense({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Ajouter Depense")),
+      body: const Center(child: Text("Page Ajouter Depense")),
+    );
+  }
+}
---
+++ lib/parametres.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Parametres extends StatelessWidget {
+  const Parametres({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Parametres")),
+      body: const Center(child: Text("Page Parametres")),
+    );
+  }
+}
