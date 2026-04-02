f9b79afbee6f520b2573309a90039a59c01540e5
lebeaugs <ianhafiz9999@gmail.com>
2026-04-02 15:14:42 +0200

    feat: intégration de Provider pour la gestion d'état et affichage de l'historique

--- lib/widgets/historique.dart
+++ lib/widgets/historique.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../models/depense_model.dart';
 
 class Historique extends StatelessWidget {
   const Historique({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final depenses = context.watch<DepenseModel>().historique;
+
     return Scaffold(
-      appBar: AppBar(title: const Text("Historique")),
-      body: const Center(child: Text("Page Historique")),
+      appBar: AppBar(
+        title: const Text("Historique"),
+      ),
+
+      body: depenses.isEmpty
+          ? const Center(
+        child: Text("Aucune dépense pour le moment"),
+      )
+          : ListView.builder(
+        itemCount: depenses.length,
+        itemBuilder: (context, index) {
+          final depense = depenses[index];
+
+          return ListTile(
+            leading: const Icon(Icons.receipt),
+            title: Text(depense.titre),
+            subtitle: Text(depense.description),
+            trailing: Text(
+              "${depense.montant.toStringAsFixed(2)} €",
+            ),
+          );
+        },
+      ),
     );
   }
 }
